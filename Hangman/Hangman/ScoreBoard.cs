16a5394c06e8fac96e52a7cfd354ca70297d0b0c
tanyta78 <tanyta78@gmail.com>
2017-06-07 11:12:01 +0300

    Some changes for score board

--- Hangman/Hangman/ScoreBoard.cs
+++ Hangman/Hangman/ScoreBoard.cs
@@ -3,72 +3,86 @@
     using System;
 
     public class ScoreBoard
-   {
-       public const int ScoresCount = 10;
+    {
+        public const int ScoresCount = 10;
 
-        private string [] bestPlayerNames = new string[ScoresCount];
+        private string[] bestPlayerNames = new string[ScoresCount];
 
         private int[] mistakes = new int[ScoresCount];
 
 
         public ScoreBoard()
         {
-            for (int i = 0; i < ScoresCount; i++)
+            for (int i = 0; i < bestPlayerNames.Length; i++)
             {
-                bestPlayerNames[i]=String.Empty;
+                bestPlayerNames[i] = null;
                 mistakes[i] = int.MaxValue;
             }
         }
 
-       public void PrintScoreBoard()
-       {
-           Console.WriteLine("Best hangmam players:");
+        public void PrintScoreBoard()
+        {
+            Console.WriteLine("Best hangmam players:");
+            int i = 0;
+            while (bestPlayerNames[i] != null)
+            {
+                Console.WriteLine(String.Format("{0}. {1,15}===> {2,3} mistakes", i + 1, bestPlayerNames[i], mistakes[i]));
+                i++;
+                if (i >= bestPlayerNames.Length)
+                {
+                    break;
+                }
 
-           for (int i = 0; i < ScoresCount; i++)
-           {
-               if (bestPlayerNames[i]!=String.Empty)
-               {
-                   Console.WriteLine(String.Format("{0}. {1,15}===> {2,3} mistakes",i+1,bestPlayerNames[i],mistakes[i]));
-               }
-           }
-       }
+            }
+        }
 
-       public void AddNewScore(string playerName, int playerMistakes)
-       {
+        public void AddNewScore(string playerName, int playerMistakes)
+        {
             //find correct place index for player's result
-           int indexInScoreBoard = -1;
-           for (int i = 0; i < mistakes.Length; i++)
-           {
-               if (playerMistakes < mistakes[i])
-               {
-                   indexInScoreBoard = i;
-               }
-           }
+          
 
-           //change scoreList
-           if (indexInScoreBoard!=-1)
-           {
-               for (int index = ScoresCount-1; index >indexInScoreBoard; index--)
-               {
-                   bestPlayerNames[index] = bestPlayerNames[index - 1];
-                   mistakes[index] = mistakes[index - 1];
-               }
-           }
+          int indexInScoreBoard = FindCorrectIndex(playerMistakes);
 
-           bestPlayerNames[indexInScoreBoard] = playerName;
-           mistakes[indexInScoreBoard] = playerMistakes;
+            //change scoreList
 
-       }
+            if (indexInScoreBoard<bestPlayerNames.Length)
+            {
+                for (int index = bestPlayerNames.Length - 1; index > indexInScoreBoard; index--)
+                {
+                    bestPlayerNames[index] = bestPlayerNames[index - 1];
+                    mistakes[index] = mistakes[index - 1];
+                }
 
-       public int GetLastPositionMistakes()
-       {
-           int worstTopScore = int.MaxValue;
-           if (bestPlayerNames[bestPlayerNames.Length - 1] != null)
-           {
-               worstTopScore = mistakes[bestPlayerNames.Length - 1];
-           }
 
-           return worstTopScore;
+                bestPlayerNames[indexInScoreBoard] = playerName;
+                mistakes[indexInScoreBoard] = playerMistakes;
+            }
+            
+
+        }
+
+        private int FindCorrectIndex(int playerMistakes)
+        {
+            for (int i = 0; i < mistakes.Length; i++)
+            {
+                if (playerMistakes < mistakes[i])
+                {
+                    return i;
+                }
+            }
+            return bestPlayerNames.Length;
+            
+        }
+
+        public int GetLastPositionMistakes()
+        {
+            int worstTopScore = int.MaxValue;
+            if (bestPlayerNames[bestPlayerNames.Length - 1] != null)
+            {
+                worstTopScore = mistakes[bestPlayerNames.Length - 1];
+            }
+
+            return worstTopScore;
         }
 
     }
